830e58f3e49c96cfe3a22f1e7c3435e34fd1d30c
vineet39 <vinit.bugtani@gmail.com>
2021-01-21 14:15:51 +1100

    Navbar with navlinks

--- reddit-web/src/pages/_app.tsx
+++ reddit-web/src/pages/_app.tsx
@@ -1,9 +1,43 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
 import theme from "../theme";
+import { createClient, dedupExchange, fetchExchange, Provider, Query } from 'urql';
+import { cacheExchange, QueryInput, Cache, query } from '@urql/exchange-graphcache';
+import { LoginMutation, MeDocument, MeQuery } from "../generated/graphql";
 
+
+function betterUpdateQuery<Result,Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+){
+    return cache.updateQuery(qi,(data) => fn(result, data as any) as any);
+}
 const client = createClient({
   url: "http://localhost:4000/graphql",
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache,
+            { query: MeDocument},
+            result,
+            (result, query) => {
+              if(result.login.errors){
+                return query;
+              }
+              else{ 
+                return{
+                  me: result.login.user
+                } 
+              }
+            }
+          );
+      }
+    }
+  }
+}), fetchExchange]
 });
 
 function MyApp({ Component, pageProps }) {
